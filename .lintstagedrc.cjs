99d778f876b6cb7ad5c36446301c5fb7b29ead77
18200722014 <956725345@qq.com>
2023-08-29 22:31:20 +0800

    build(package.json): 添加commitlinst

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   '*.{js,ts}': [
     'eslint --fix', // 通过eslint重新格式化代码
-    'prettier --write' // 通过prettier重新格式化代码
+    'prettier --write' // 通过prettier重新格式化代码---
   ],
   '*.{cjs,json}': ['prettier --write'],
   '*.{vue,html}': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
