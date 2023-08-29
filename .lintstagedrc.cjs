21d4d64379fdbca51be2341d10876e086f118b91
18200722014 <956725345@qq.com>
2023-08-29 22:02:26 +0800

    123123

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  '*.{js,ts}': [
+    'eslint --fix', // 通过eslint重新格式化代码
+    'prettier --write' // 通过prettier重新格式化代码
+  ],
+  '*.{cjs,json}': ['prettier --write'],
+  '*.{vue,html}': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
+  '*.{scss,css}': ['stylelint --fix', 'prettier --write'],
+  '*.md': ['prettier --write']
+}
