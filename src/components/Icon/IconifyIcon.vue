4ac218651c2ef1e32d8fc14974f857aee6e7dd7c
18200722014 <956725345@qq.com>
2023-09-07 23:00:58 +0800

    refactor(icon): 重构了icon图标组件的封装以及添加了头部右侧头像等静态布局

---
+++ src/components/Icon/IconifyIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <IconifyIcon :icon="iconName" :style="{ ...attr }" />
+</template>
+
+<script setup lang="ts">
+import { Icon as IconifyIcon } from '@iconify/vue'
+import { useAttrs } from 'vue'
+
+defineOptions({
+  name: 'IconifyIcon'
+})
+
+/** 父组件绑定在子组件上但子组件没有使用props接收的属性 */
+const attr = useAttrs()
+console.log(attr)
+
+const props = withDefaults(defineProps<{ iconName: string }>(), {
+  iconName: ''
+})
+
+console.log(props)
+</script>
+
+<style lang="scss" scoped></style>
--- src/layout/Header/components/Nav.vue
+++ src/layout/Header/components/Nav.vue
@@ -4,22 +4,47 @@
     <div class="nav-left">
       <!-- 折叠图标 -->
       <i class="nav-left__iscollapse">
-        <component :is="renderIconIfyIcon('ant-design:menu-fold-outlined')" />
+        <iconify-icon icon-name="ant-design:menu-fold-outlined"></iconify-icon>
       </i>
       <!-- 面包屑 -->
       <Breadcrumb />
     </div>
     <!-- 右侧区域 -->
-    <div class="nav-right"></div>
+    <ul class="nav-right">
+      <li class="nav-right__item">
+        <i>
+          <iconify-icon icon-name="fe:search"></iconify-icon>
+        </i>
+      </li>
+      <li class="nav-right__item">
+        <i>
+          <iconify-icon icon-name="ant-design:bell-outlined"></iconify-icon>
+        </i>
+      </li>
+      <li class="nav-right__item">
+        <i>
+          <iconify-icon icon-name="icon-park:full-screen"></iconify-icon>
+        </i>
+      </li>
+      <li class="nav-right__item">
+        <el-avatar :size="35" :src="getImageUrl('logo.png')" />
+      </li>
+      <li class="nav-right__item">
+        <i>
+          <iconify-icon icon-name="uil:setting"></iconify-icon>
+        </i>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-/** 创建icon */
-import { renderIconIfyIcon } from '@/utils/index'
 /** 引入面包屑组件 */
 import Breadcrumb from './Breadcrumb.vue'
-
+/** 引入iconify-icon */
+import IconifyIcon from '@/components/Icon/IconifyIcon.vue'
+/** 引入图片工具函数 */
+import { getImageUrl } from '@/utils/index'
 defineOptions({
   name: 'DTNav'
 })
@@ -39,9 +64,25 @@ defineOptions({
     align-items: center;
 
     &__iscollapse {
+      display: flex;
       margin-right: 10px;
       font-size: 18px;
       cursor: pointer;
+    }
+  }
+
+  &-right {
+    display: flex;
+    align-items: center;
+
+    &__item {
+      margin-right: 15px;
+      cursor: pointer;
+
+      i {
+        display: flex;
+        font-size: 18px;
+      }
     }
   }
 }
